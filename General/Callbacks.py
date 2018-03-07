8b2fc6f6823486c191e43e1d185ea65dae2e2e27
GilesStrong <giles.strong@outlook.com>
2018-03-07 17:24:44 +0100

    Adding CosineAnnealing

--- General/Callbacks.py
+++ General/Callbacks.py
@@ -75,3 +75,43 @@ class LRFinder(Callback):
                 print('Ending training early due to loss increase')
                 self.model.stop_training = True
         if (loss<self.best and batch>10): self.best=loss
+
+class CosAnneal(Callback):
+    '''Adapted from fastai version'''
+    def __init__(self, nb, cycle_mult=1):
+        super(CosAnneal, self).__init__()
+        self.nb = nb
+        self.cycle_mult = cycle_mult
+        self.cycle_iter = 0
+        self.cycle_count = 0
+        self.lrs = []
+        self.lr = -1
+
+    def on_train_begin(self, logs={}):
+        if self.lr == -1:
+            self.lr = float(K.get_value(self.model.optimizer.lr))
+        
+    def plot_lr(self):
+        plt.figure(figsize=(16,8))
+        plt.xlabel("iterations")
+        plt.ylabel("learning rate")
+        plt.plot(range(len(self.lrs)), self.lrs)
+        plt.show()
+        
+    def calc_lr(self, batch):
+        '''if batch < self.nb/20:
+            self.cycle_iter += 1
+            return self.lr/100.'''
+
+        cos_out = np.cos(np.pi*(self.cycle_iter)/self.nb) + 1
+        self.cycle_iter += 1
+        if self.cycle_iter==self.nb:
+            self.cycle_iter = 0
+            self.nb *= self.cycle_mult
+            self.cycle_count += 1
+        return self.lr / 2 * cos_out
+
+    def on_batch_end(self, batch, logs={}):
+        lr = self.calc_lr(batch)
+        self.lrs.append(lr)
+        K.set_value(self.model.optimizer.lr, lr)
