42f472e6154a0201fd17f1a3e3bff92f82f2fca9
GilesStrong <giles.strong@outlook.com>
2018-03-15 12:26:16 +0000

    Adding more control to lrfinder history plotter

--- General/Callbacks.py
+++ General/Callbacks.py
@@ -44,7 +44,7 @@ class LRFinder(Callback):
     def calc_lr(self, lr, batch):
         return self.lrBounds[0]*(self.lr_mult**batch)
     
-    def plot(self, n_skip=0, n_max=0):
+    def plot(self, n_skip=0, n_max=-1):
         plt.figure(figsize=(16,8))
         plt.ylabel("loss")
         plt.xlabel("learning rate (log scale)")
