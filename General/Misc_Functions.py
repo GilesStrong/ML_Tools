4d4d0ad3912ef7cd553069793ee6a6f1d701c203
GilesStrong <giles.strong@outlook.com>
2018-10-18 14:49:09 +0100

    improvign uncertround for >1 uncertainties

--- General/Misc_Functions.py
+++ General/Misc_Functions.py
@@ -3,12 +3,20 @@ from sklearn.model_selection import train_test_split
 def uncertRound(value, uncert):
     if uncert == 0:
         return value, uncert
+    
+    factor = 1.0
+    while uncert/factor > 1:
+        factor *= 10.0
+    
+    value /= factor
+    uncert /= factor
+        
     i = 0    
     while uncert*(10**i) <= 1:
         i += 1
-
-    roundUncert = round(uncert, i)
-    roundValue = round(value, i)
+    
+    roundUncert = factor*round(uncert, i)
+    roundValue = factor*round(value, i)
     if int(roundUncert) == roundUncert:
         roundUncert = int(roundUncert)
         roundValue = int(roundValue)
