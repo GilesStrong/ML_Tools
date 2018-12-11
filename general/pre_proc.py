d9bae3480621b706c5c1382f25ce7f8fb610253b
GilesStrong <giles.strong@outlook.com>
2018-12-11 15:18:56 +0300

    Axpanding preproc pipes

--- general/pre_proc.py
+++ general/pre_proc.py
@@ -3,7 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
 
 
-def get_pre_proc_pipes(norm_in=False, norm_out=False, pca=False, whiten=False):
+def get_pre_proc_pipes(norm_in=False, norm_out=False, pca=False, whiten=False, with_mean=True, with_std=True):
     steps_in = []
     if not norm_in and not pca:
         steps_in.append(('ident', StandardScaler(with_mean=False, with_std=False)))  # For compatability
@@ -11,11 +11,11 @@ def get_pre_proc_pipes(norm_in=False, norm_out=False, pca=False, whiten=False):
         if pca:
             steps_in.append(('pca', PCA(whiten=whiten)))
         if norm_in:
-            steps_in.append(('norm_in', StandardScaler()))
+            steps_in.append(('norm_in', StandardScaler(with_mean=with_mean, with_std=with_std)))
     input_pipe = Pipeline(steps_in)
     steps_out = []
     if norm_out:
-        steps_out.append(('norm_out', StandardScaler()))
+        steps_out.append(('norm_out', StandardScaler(with_mean=with_mean, with_std=with_std)))
     else:
         steps_out.append(('ident', StandardScaler(with_mean=False, with_std=False)))  # For compatability
     output_pipe = Pipeline(steps_out)
